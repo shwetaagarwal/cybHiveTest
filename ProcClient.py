b3d7960eb4e5d3f49136f963924a4362930758cc
shwetaagarwal <shweta2209@gmail.com>
2020-07-26 11:08:31 +0100

    Add files via upload

---
+++ ProcClient.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3 -x
+#client
+
+import socket
+from struct import pack
+import subprocess
+import time
+
+HOST = '127.0.0.1'  # The server's hostname or IP address
+PORT = 65432        # The port used by the server
+
+def send_data():
+    #command = ['ps -eas | tr -s \" \" | cut -d \" \" -f 2,5']
+    #out = subprocess.run(command, stdout=subprocess.PIPE)
+    out = subprocess.run(["ps", "-eas"], stdout=subprocess.PIPE)
+    if(0!=out.returncode):
+        print("The exit code was: %d" % out.returncode)
+        exit(1)
+    else:
+        process_data = out.stdout
+        print(process_data)
+        length = pack('>Q', len(process_data))
+        s.sendall(length)
+        s.sendall(process_data)
+        print('sent %d' % len(process_data) )
+        ack = s.recv(1)
+        print('Received', repr(ack))
+
+def sendPeriodically():    
+    send_data()
+    time.sleep(5.0)
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    try:
+        s.connect((HOST, PORT))
+    except OSError as error:
+        print("Couldnt connect with the socket-server: %s\n terminating program" % error.strerror)
+        exit(1)
+    while(True):
+        sendPeriodically()   
+    
+    
+   
+
---
+++ ProcSrv.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import socket
+import os
+import sys
+from struct import unpack
+
+HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
+PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+
+
+output_dir = '.'
+file_num = 1
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    print('Server...')
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print('Connected by', addr)
+        while True:
+            try:
+                dataLenBin = conn.recv(8)
+                #raise Exception("Testing exception")
+            except OSError as error:
+                print("Couldnt recv: %s\n terminating program" % error.strerror)
+                continue
+            if not dataLenBin:
+                break
+            (dataLen,) = unpack('>Q', dataLenBin)
+            data = b''
+            print(dataLen)
+            while len(data) < dataLen:
+                to_read = dataLen - len(data)
+                data += conn.recv(4096 if to_read > 4096 else to_read)
+            assert len(b'\00') == 1
+            conn.sendall(b'\00')
+            print('Sent reply')
+            with open(os.path.join(output_dir, 'ProcList%02d.txt' % file_num), 'w') as fp:
+                fp.write(data.decode(sys.stdout.encoding)) 
